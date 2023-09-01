e8ae3ebc065c5a30897c1a556e7a183cb0f4ef67
wendtl <lwendt220@gmail.com>
2023-08-31 22:45:15 -0500

    make it unnecessary portable

--- index.ts
+++ index.ts
@@ -1,7 +1,18 @@
 import puppeteer from 'puppeteer'
 
-(async () => {
-  // Launch the browser and open a new blank page
+const BACK_GLASS = 275 // Hardcoded part number
+
+
+/**
+ * Query the most(?) popular junkyard in Minnesota's online inventory for parts that fit a specific vehicle
+ * @param year the model year of the vehicle
+ * @param make the brand of the vehicle
+ * @param model the model name of the vehicle
+ * @param partId references an arbitrary part number set by the U Pull website (sorry)
+ * @returns 2-dimensional list of in-stock vehicles that may have compatible parts
+ */
+const getUPullInventory = async (year: number, make: string, model: string, partId: number) => {
+  // Launch browser and open a new page
   const browser = await puppeteer.launch({ headless: 'new' });
   const page = await browser.newPage();
 
@@ -14,19 +25,19 @@ import puppeteer from 'puppeteer'
   await page.click('#searchPartButton');
   process.stdout.write('Selecting Vehicle')
   await page.waitForNetworkIdle()
-  await page.select('#years', '2009')
+  await page.select('#years', year.toString())
 
   process.stdout.write('.')
   await page.waitForNetworkIdle()
-  await page.select('#makes', 'Honda')
+  await page.select('#makes', make)
 
   process.stdout.write('.')
   await page.waitForNetworkIdle()
-  await page.select('#models', 'PILOT')
+  await page.select('#models', model.toUpperCase())
 
   process.stdout.write('.')
   await page.waitForNetworkIdle()
-  await page.select('#parts', '275')
+  await page.select('#parts', partId.toString())
 
   process.stdout.write('.\n')
   await page.click('#icSubmit')
@@ -47,11 +58,12 @@ import puppeteer from 'puppeteer'
     });
   })
 
-  // TODO: maybe do something more useful with the output
-  console.log(rows)
-
   // cleanup
   await browser.close()
 
   return rows
-})();
+};
+
+// I need a new tailgate, lets see whats available
+const results = await getUPullInventory(2009, 'Honda', 'Pilot', BACK_GLASS)
+console.log(results)
